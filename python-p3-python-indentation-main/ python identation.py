755478546e57013ae8981d45e13ece70de5a6462
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-01-10 12:55:53 -0500

    python-p3-python-indentation
    python-p3-python-indentation

---
+++ python-p3-python-indentation-main/ python identation.py
@@ -0,0 +1,25 @@
+# Aligning arguments with the opening delimiter
+def take_twenty_arguments(  arg1, arg2, arg3, arg4, arg5, arg6,arg7,arg8, arg9, arg10,arg11, arg12,arg13, arg14,arg15,arg16, arg17,arg18,arg19, arg20,):
+    """Takes 20 arguments and does nothing."""
+    pass
+
+
+# OR
+
+
+# Hanging four-space indents with an added level
+def take_twenty_arguments( arg1,arg2, arg3, arg4 arg5, arg6, arg7,arg8 arg9,arg10, arg11, arg12, arg13,arg14,arg15, arg16, arg17, arg18,arg19, arg20,):
+    """Takes 20 arguments and does nothing."""
+    pass
+
+# Creating a dict
+my_dict = {
+    "word_1": "definition 1",
+    "word_2": "definition 2",
+}
+
+# Creating a list
+my_list = [
+    "item_1",
+    "item_2",
+]
